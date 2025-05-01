ac39f1394edd8cee559371acf37118096a283801
John Arban Lewis <johnarban@users.noreply.github.com>
2025-05-01 17:10:15 -0400

    working code

---
+++ get_class_from_db.py
@@ -0,0 +1,102 @@
+from collections import Counter
+from typing import cast, Optional
+from qualtrics_keys import database_keys
+
+import pandas as pd
+
+import mysql.connector
+
+
+def connect_to_db(database_dict=None):
+    # connect to the database
+    if database_dict is None:
+        database_dict = {
+            "host": "localhost",
+            "user": "user",
+            "password": "password",
+            "database": "database",
+        }
+    mydb = mysql.connector.connect(
+        host=database_dict["host"],
+        user=database_dict["user"],
+        passwd=database_dict["passwd"],
+        database=database_dict["database"],
+    )
+    return mydb
+
+
+# function to run an sql query and return the results as a pandas dataframe
+def run_sql_query(sql_query, database_dict=None, return_df=True):
+    # connect to the database
+    mydb = connect_to_db(database_dict=database_dict)
+    # run the query
+    mycursor = mydb.cursor()
+    mycursor.execute(sql_query)
+    # get the results
+    results = cast(list[dict], mycursor.fetchall())
+    # get the column names
+    col_names = [desc[0] for desc in mycursor.description]  # type: ignore
+    # close the connection
+    mydb.close()
+    # return the results as a pandas dataframe
+    if return_df:
+        return pd.DataFrame(results, columns=col_names, dtype=object)
+    else:
+        return [{col_names[i]: res[i] for i in range(len(res))} for res in results]
+
+
+sql_query = """
+SELECT  
+    Educators.first_name, 
+    Educators.last_name,
+    Classes.id, 
+    Classes.name
+FROM Classes
+JOIN StudentsClasses on StudentsClasses.class_id = Classes.id
+JOIN Educators on Educators.id = Classes.educator_id
+WHERE StudentsClasses.student_id={student_id}
+"""
+
+
+def get_student_class_info(student_id) -> pd.DataFrame | None:
+    try:
+        out = cast(
+            pd.DataFrame,
+            run_sql_query(
+                sql_query.format(student_id=student_id), database_keys, return_df=True
+            ),
+        )
+        return out
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+
+# Function to get class information for a list of student IDs
+def get_students_classes_info(student_ids: list) -> pd.DataFrame | None:
+    try:
+        # Modify the SQL query to use IN clause for multiple student IDs
+        sql_query_multiple = """
+        SELECT  
+            Educators.first_name, 
+            Educators.last_name,
+            Classes.id, 
+            Classes.name,
+            StudentsClasses.student_id
+        FROM Classes
+        JOIN StudentsClasses on StudentsClasses.class_id = Classes.id
+        JOIN Educators on Educators.id = Classes.educator_id
+        WHERE StudentsClasses.student_id IN ({student_ids})
+        """
+        # Format the query with the list of student IDs
+        formatted_query = sql_query_multiple.format(
+            student_ids=",".join(map(str, student_ids))
+        )
+        # Run the query and return the results
+        out = cast(
+            pd.DataFrame, run_sql_query(formatted_query, database_keys, return_df=True)
+        )
+        return out
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
---
+++ qualtrics_api.py
@@ -0,0 +1,139 @@
+import requests
+from qualtrics_keys import token, data_center, survey_id
+
+
+# https://api.qualtrics.com/6b00592b9c013-start-response-export
+# Start a response export
+
+
+def start_response_export(token, data_center, survey_id, format = "csv", numeric=False):
+    
+    start_url = "https://{data_center}.qualtrics.com/API/v3/surveys/{survey_id}/export-responses".format(data_center=data_center, survey_id=survey_id)
+
+    
+    if format == "csv":
+        payload = { 
+            "format": "csv" ,
+            "compress": False,
+            "useLabels": not numeric,
+            "surveyMetadataIds": ["duration"]
+            }
+    elif format == "json":
+        payload = { 
+            "format": "json" ,
+            "compress": False,
+            "surveyMetadataIds": ["duration"]
+            }
+        
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "X-API-TOKEN": token
+    }
+
+    response = requests.post(start_url, json=payload, headers=headers)
+
+
+    if response.status_code != 200:
+        print("Error: ", response.status_code)
+        print(response.text)
+        return False, None, None
+    else:
+        start_result = response.json()
+        export_progress_id = start_result['result']['progressId']
+        print("Export progress ID: ", export_progress_id)
+        
+    return True, export_progress_id, start_result
+
+from typing import Literal
+
+def check_response_export(token, data_center, survey_id, export_progress_id) -> str | Literal[False]:
+    check_url = "https://{data_center}.qualtrics.com/API/v3/surveys/{survey_id}/export-responses/{export_progress_id}".format(data_center=data_center, survey_id=survey_id, export_progress_id = export_progress_id)
+
+    
+    headers = {
+        "Accept": "application/json",
+        "X-API-TOKEN": token
+    }
+
+    response = requests.get(check_url, headers=headers)
+
+    if response.status_code != 200:
+        print("Error: ", response.status_code)
+        print(response.text)
+        return False
+    else:
+        check_result = response.json() 
+        if 'result' in check_result:
+            if 'status' in check_result['result']:
+                if check_result['result']['status'] == 'complete':
+                    print("fileId: ", check_result['result']['fileId'])
+                    return check_result['result']['fileId']
+                elif 'percentComplete' in check_result['result']:
+                    print("Percent complete: ", check_result['result']['percentComplete'])
+                    return False
+            
+    return False
+
+
+def download_response_export(token, data_center, survey_id, file_id):
+    requestDownloadUrl = "https://{data_center}.qualtrics.com/API/v3/surveys/{survey_id}/export-responses/{file_id}/file".format(data_center=data_center, survey_id=survey_id, file_id = file_id)
+    headers = {
+        "Accept": "application/json",
+        "X-API-TOKEN": token
+    }
+    response = requests.request("GET", requestDownloadUrl, headers=headers, stream=True)
+
+    if response.status_code != 200:
+        print("Error in download: ", response.status_code)
+
+    return response
+
+import time
+def exponential_backoff(func, *args, **kwargs):
+    """
+    Retry a function with exponential backoff.
+    """
+    max_retries = 5
+    for i in range(max_retries):
+        out = func(*args, **kwargs)
+        if out == False:
+            print(f"Error: Retrying in {2 ** i} seconds...")
+            time.sleep(2 ** i)
+        else:
+            return out
+    raise Exception("Max retries exceeded")
+
+
+from io import StringIO
+def get_survey(token, data_center, survey_id, format="csv", numeric=False):
+    """
+    Get survey responses from Qualtrics API.
+    """
+    # Start the response export
+    success, export_progress_id, start_result = start_response_export(token, data_center, survey_id, format=format, numeric=numeric)
+    
+    
+    if not success:
+        print("Failed to start response export.")
+        return
+
+    # Check the response export status
+    file_id = check_response_export(token, data_center, survey_id, export_progress_id)
+    max_tries = 3
+    while file_id == False and max_tries > 0:
+        print("Waiting for response export to complete...")
+        file_id = exponential_backoff(check_response_export, token, data_center, survey_id, export_progress_id)
+        max_tries -= 1
+    
+    
+    if file_id == False:
+        print("Failed to get response export.")
+        return
+        # Download the response export
+    download_result = download_response_export(token, data_center, survey_id, file_id)
+    if download_result.status_code != 200:
+        print("Error in download: ", download_result.status_code)
+        return
+    return download_result.content.decode('utf-8')
+
